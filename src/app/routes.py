626481598ff62b6efb65a03e5f198f6d7c8c54db
Khalil Collins <Collinskhalil@hotmail.com>
2023-02-22 15:57:13 -0500

    feat(components): add timelinepost routes and timeline page route

--- src/app/routes.py
+++ src/app/routes.py
@@ -1,12 +1,14 @@
 from flask import render_template, url_for
 from app import app
+from app import TimelinePost
 import os
 from .models.experience import Experience
 from .models.education import Education
 from .models.hobbies import Hobbies
 from .models.about import About
 from .static.sample_data.data import data
-    
+from flask import request
+from playhouse.shortcuts import model_to_dict
 @app.route('/')
 def index():
     return render_template('layout.html',
@@ -15,7 +17,7 @@ def index():
                            url=os.getenv("URL")
                            )
 
-@app.route('/<name>')
+@app.route('/about/<name>')
 def about(name):
     aboutme = []
     for about in data[name]['about']:
@@ -87,3 +89,38 @@ def hobbie(name):
                            url=os.getenv("URL"),
                            type='Hobbies', 
                            elements=hobbies)
+@app.route('/timeline/')
+def timeline():
+    return render_template('timeline.html', title="Timeline")
+
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
+    
+    return model_to_dict(timeline_post)
+
+@app.route('/api/timeline_post', methods=['GET'])
+def get_time_line_post():
+    return {
+        'timeline_posts':[
+        model_to_dict(p)
+        for p in
+        TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
+@app.route('/api/timeline_post', methods=['DELETE'])
+def delete_time_line_posts():
+    delete_posts = TimelinePost.delete()
+    delete_posts.execute()
+    return {        'timeline_posts':[
+        model_to_dict(p)
+        for p in
+        TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+
+    }
+
+
