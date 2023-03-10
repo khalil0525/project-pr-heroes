1ff04c3cbc79ddc7fccebe7a526ba826310d2429
Khalil Collins <Collinskhalil@hotmail.com>
2023-03-10 14:43:35 -0500

    implemented docker files

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -10,7 +10,7 @@ mydb = MySQLDatabase(os.getenv('MYSQL_DATABASE'),
               user=os.getenv("MYSQL_USER"),
               password=os.getenv('MYSQL_PASSWORD'),
               host=os.getenv('MYSQL_HOST'),
-              port=3306)
+              port=4000)
 
 class TimelinePost(Model):
     name = CharField()
