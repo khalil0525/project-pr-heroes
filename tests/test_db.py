41161f499849e7653b4becda7d1cc45583378f9f
Khalil Collins <Collinskhalil@hotmail.com>
2023-03-14 16:11:41 -0400

    Fixed database model import

--- tests/test_db.py
+++ tests/test_db.py
@@ -2,7 +2,7 @@ import unittest
 from peewee import *
 from playhouse.shortcuts import model_to_dict
 
-from src.models.timelinepost import TimelinePost
+from src.app.models.timelinepost import TimelinePost
 
 MODELS = [TimelinePost]
 
